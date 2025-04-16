18c25c85b69dcfa9228ca01426e617052e4a58b1
ChrisTobing <sc22cjt@leeds.ac.uk>
2025-04-16 20:53:41 +0100

    Initial Socket Connection

--- backend/server.js
+++ backend/server.js
@@ -1,16 +1,16 @@
 const io = require("socket.io")({
-    cors: {
-        origin: '*',
-        methods: ["GET", "POST"]
-    }
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
 });
 
-io.on('connect', socket => {
-    console.log(`Socket connected: ${socket.id}`)
-    socket.on("player-ready", (message) => {
-        console.log(`Socket ready: ${socket.id}`)
-    })
-})
+io.on("connect", (socket) => {
+  console.log(`Socket connected: ${socket.id}`);
+  socket.on("player-ready", () => {
+    console.log(`Socket ready: ${socket.id}`);
+  });
+});
 
-io.listen(process.env.PORT || 3000);
-console.log(`LISTENING ON: ${process.env.PORT || 3000}`)
+io.listen(process.env.PORT || 4000);
+console.log(`LISTENING ON: ${process.env.PORT || 4000}`);
