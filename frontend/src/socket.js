dc30fd4cae50761f72f804f10b26f830fbfe9ed9
eansengchang <76442284+eansengchang@users.noreply.github.com>
2025-04-27 19:09:05 +0100

    Update socket.js

--- frontend/src/socket.js
+++ frontend/src/socket.js
@@ -2,6 +2,6 @@ import { io } from "socket.io-client";
 
 const BACKEND_IP = process.env.BACKEND_IP
 
-export const socket = io("BACKEND_IP", {
+export const socket = io(BACKEND_IP, {
   autoConnect: true,
 });
